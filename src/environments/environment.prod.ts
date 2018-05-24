05d5426c46a118f2476f1036e3a18a15cd3bc618
acint <jchmok@gmail.com>
2018-05-23 21:32:52 -0400

    Revert "Revert "Update environment properties""
    This reverts commit 2e1251f9ebdfc90ed4f0e05c9f32dcd092d1c604.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase : {
-    apiKey: 'AIzaSyBCCE-E1VEhw5fKxUCitzAK-Pp9wCfyDv4',
-    authDomain: 'meefinancial-201001.firebaseapp.com',
-    databaseURL: 'https://meefinancial-201001.firebaseio.com',
-    projectId: 'meefinancial-201001',
-    storageBucket: 'meefinancial-201001.appspot.com',
-    messagingSenderId: '464484736414'
-  }
+  production: true
 };
