0fe2fc599252a33ba1b5a2b553f0beab15881dc1
Ricky Lai <r.74259@gmail.com>
2018-06-03 14:52:16 -0400

    #2 - Create Admin page

--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -2,22 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import { User } from './data-model';
+import { registrationCollection } from '../app.database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
-  registrationCollection: AngularFirestoreCollection<User>;
+  collection: AngularFirestoreCollection<User>;
   registrations: Observable<User[]>;
 
   constructor(private afs: AngularFirestore) {
-    this.registrationCollection = afs.collection<User>('registration');
-    // this.registrations = this.registrationCollection.valueChanges();
+    this.collection = afs.collection<User>(registrationCollection);
+    // this.registrations = this.collection.valueChanges();
   }
 
   save(user: User) {
     console.log('Saving User:', user);
-    this.registrationCollection.add(user);
+    this.collection.add(user);
   }
 }
