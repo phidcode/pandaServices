55a9ded91f8fd60ac30d54bda694e94fef639c70
aaronhehehaha <dha64@sfu.ca>
2018-07-28 00:48:18 -0400

    Mortgage-Builder UI Changes and bind lender options to backend.

--- src/app/mortgage-builder/mortgage-heat-map/mortgage-heat-map.component.ts
+++ src/app/mortgage-builder/mortgage-heat-map/mortgage-heat-map.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { MortgageBuilderService} from '../mortgage-builder.service';
 import { Mortgage, Purpose, OtherDebts, PersonalInfo, JobInfo, PropertyInfo } from '../data-model';
+
+
+export interface Lender {
+  term: number; 
+  type: string; 
+  rate: string;
+}
+
 @Component({
   selector: 'app-mortgage-heat-map',
   templateUrl: './mortgage-heat-map.component.html',
   styleUrls: ['./mortgage-heat-map.component.css']
 })
+
 export class MortgageHeatMapComponent implements OnInit, OnChanges {
 
   @Input() formGroup: FormGroup;
@@ -14,6 +23,33 @@ export class MortgageHeatMapComponent implements OnInit, OnChanges {
   mortgage: Mortgage;
 
   constructor(private mbs: MortgageBuilderService) { }
+
+  displayedColumns: string[] = ['term', 'type', 'rate'];
+
+  lenderA: Lender[] = [
+    {term: 1, type: 'Fixed', rate: '2.79%'}, 
+    {term: 2, type: 'Fixed', rate: '3.14%'}, 
+    {term: 3, type: 'Fixed', rate: '3.20%'}, 
+    {term: 4, type: 'Fixed', rate: '2.89%'}, 
+    {term: 5, type: 'Fixed', rate: '3.04%'}, 
+    {term: 3, type: 'Variable', rate: '3.56%'}, 
+    {term: 5, type: 'Variable', rate: '2.46%'}, 
+  ]
+
+  lenderB: Lender[] = [
+    {term: 1, type: 'Fixed', rate: '3.09%'}, 
+    {term: 2, type: 'Fixed', rate: '3.34%'}, 
+    {term: 3, type: 'Fixed', rate: '3.53%'}, 
+    {term: 4, type: 'Fixed', rate: '3.09%'}, 
+    {term: 5, type: 'Fixed', rate: '4.53%'}, 
+    {term: 3, type: 'Variable', rate: '3.50%'}, 
+    {term: 5, type: 'Variable', rate: '3.20%'}, 
+  ]
+
+  lenderC: Lender[] = [
+    {term: 1, type: 'Fixed', rate: '7.33%'}, 
+    {term: 2, type: 'Fixed', rate: 'Call Us'}, 
+  ]
 
   ngOnInit() {
   }
