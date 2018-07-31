9996698f806f63bf04e2ac34cf08d0d192773ce4
aaronhehehaha <dha64@sfu.ca>
2018-07-31 15:24:06 -0400

    FAQ translation added

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from "@angular/platform-browser";
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
@@ -7,11 +8,14 @@ import { Meta, Title } from "@angular/platform-browser";
 })
 export class FaqComponent implements OnInit {
 
-  constructor(private meta: Meta,title: Title) {    
+  constructor(private meta: Meta,title: Title, public translate: TranslateService) {    
     title.setTitle('FAQs - MeeFinancial Inc., Your Digital Mortgage');    
     this.meta.addTag({ name: 'description', content: 'Buying a home? We can help. Get pre-approved and qualify for a mortgage.' },true);    
     this.meta.addTag({ name: 'author', content: 'MeeFinancial Inc.' }, true);
     this.meta.addTag({ name: 'keywords', content: 'faq mortgage, toronto mortgage, canada mortgage, DIY mortgage, real estate, buying home, ontario real estate, toronto mortgage, diy mortgage' }, true);    
+  
+    translate.addLangs(['en', 'zh']);
+    translate.setDefaultLang('en');
   }
 
 
