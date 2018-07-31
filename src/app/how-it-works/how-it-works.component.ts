653ce1dfbecb10135d7016b6105049323cdbd22b
aaronhehehaha <dha64@sfu.ca>
2018-07-31 15:20:34 -0400

    How-it-works translation added

--- src/app/how-it-works/how-it-works.component.ts
+++ src/app/how-it-works/how-it-works.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from "@angular/platform-browser";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-how-it-works',
@@ -8,11 +9,14 @@ import { Meta, Title } from "@angular/platform-browser";
 })
 export class HowItWorksComponent implements OnInit {
 
-  constructor(private meta: Meta,title: Title) {    
+  constructor(private meta: Meta,title: Title, public translate: TranslateService) {    
     title.setTitle('How It Works - MeeFinancial Inc., Your Digital Mortgage');    
     this.meta.addTag({ name: 'description', content: 'Buying a home? We can help. Get pre-approved and qualify for a mortgage.' },true);    
     this.meta.addTag({ name: 'author', content: 'MeeFinancial Inc.' }, true);
-    this.meta.addTag({ name: 'keywords', content: 'mortgage, toronto mortgage, canada mortgage, DIY mortgage, real estate, buying home, ontario real estate, toronto mortgage, diy mortgage' }, true);    
+    this.meta.addTag({ name: 'keywords', content: 'mortgage, toronto mortgage, canada mortgage, DIY mortgage, real estate, buying home, ontario real estate, toronto mortgage, diy mortgage' }, true);
+    
+    translate.addLangs(['en', 'zh']);
+    translate.setDefaultLang('en'); 
   }
 
 
